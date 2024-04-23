e421c4e11d65ffebba145915557efc8c3971f953
evolify <wolf@evolify.cn>
2024-04-23 11:24:05 +0800

    chore: add vercel analytics

--- app/main/main.tsx
+++ app/main/main.tsx
@@ -1,3 +1,5 @@
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import Theme from "common/ui/theme/index"
 import { render } from "common/utils"
 import App from "./App"
@@ -6,5 +8,7 @@ import "./style.scss"
 render(
   <Theme>
     <App />
+    <Analytics />
+    <SpeedInsights />
   </Theme>
 )
