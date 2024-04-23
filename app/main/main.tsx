f3f99f5c84ded04f764e3ab1e188a1c3dd28d0d3
evolify <wolf@evolify.cn>
2024-04-23 12:47:48 +0800

    chore: update speed-insights

--- app/main/main.tsx
+++ app/main/main.tsx
@@ -1,5 +1,5 @@
 import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import Theme from "common/ui/theme/index"
 import { render } from "common/utils"
 import App from "./App"
