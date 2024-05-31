dfc785b6c184340f1394eebc0d2349a6a9addb7b
evolify <wolf@evolify.cn>
2024-05-31 14:47:08 +0800

    feat: update data cache

--- common/api/index.ts
+++ common/api/index.ts
@@ -1,7 +1,7 @@
 import { PumpCoin } from "common/types"
 
 export async function getPumpList(): Promise<PumpCoin[]>{
-  const res = await fetch("https://gmgn.ai/defi/quotation/v1/rank/sol/pump?limit=50&orderby=progress&direction=desc&pump=true")
+  const res = await fetch("https://gmgn.ai/defi/quotation/v1/rank/sol/pump?limit=50&orderby=progress&direction=desc&pump=true", {cache: "no-store"})
   const data = await res.json()
   if(data.code === 0){
     return data.data.rank
