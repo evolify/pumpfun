f42057cae9d869b2b3ab0193495329b3f84433c6
evolify <wolf@evolify.cn>
2025-03-17 15:44:34 +0800

    feat: enhance store functionality and update API base URL

--- app/store.ts
+++ app/store.ts
@@ -1,10 +1,17 @@
 import Store from "common/utils/store"
 import { PumpCoin } from "common/types"
+import { getPumpList } from "common/api"
 
 
 export const { state, update, use } = new Store({
-  coin: null as PumpCoin
+  coin: null as PumpCoin,
+  list: [] as PumpCoin[]
 })
+
+export async function load() {
+  const list = await getPumpList()
+  update({ list })
+}
 
 export function open(coin: PumpCoin){
   update({coin})
--- common/api/index.ts
+++ common/api/index.ts
@@ -1,22 +1,16 @@
 import { PumpCoin, PumpDetail } from "common/types"
 
-const BASE_URL = 'https://ngapi.vercel.app/api/ngmg'
+// const BASE_URL = 'https://ngapi.vercel.app/api/ngmg'
+const BASE_URL = '/ngmg/api'
 
 export async function getPumpList(): Promise<PumpCoin[]>{
   const res = await fetch(`${BASE_URL}/list`, {cache: "no-store"})
   const data = await res.json()
-  console.log(data)
-  if(data.code === 0){
-    return data.rank
-  }
-  return []
+  return data.rank
 }
 
 export async function getPumpDetail(addr: string): Promise<PumpDetail>{
   const res = await fetch(`${BASE_URL}/detail?address=${addr}`)
   const data = await res.json()
-  console.log(data)
-  if(data.code === 0){
-    return data
-  }
+  return data
 }
