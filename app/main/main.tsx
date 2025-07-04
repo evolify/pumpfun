b4678debcda7582b5ee8fc76a81fee9a009343e1
evolify <wolf@evolify.cn>
2025-07-04 18:18:01 +0800

    feat: add gtm

--- app/main/main.tsx
+++ app/main/main.tsx
@@ -3,6 +3,12 @@ import { render, fetcher } from "@/utils"
 import { SWRConfig } from "swr"
 import "@/styles/common.css"
 
+import TagManager from "react-gtm-module"
+
+TagManager.initialize({
+  gtmId: "GTM-WM9RJCSR",
+})
+
 render(
   <SWRConfig
     value={{
