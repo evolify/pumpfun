22d4cc9aec5cc5e1839f2e4bf57b44f41a8b06e2
evolify <wolf@evolify.cn>
2025-07-07 18:12:56 +0800

    feat: add page entry

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,27 @@ import react from "@vitejs/plugin-react"
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 
+function getInput(...dirs: string[]) {
+  const input: Record<string, string> = {
+    main: "app/index.html",
+  }
+  dirs.forEach(t => {
+    input[t] = `app/${t}/index.html`
+  })
+  return input
+}
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./common"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: getInput("launchpad"),
     },
   },
 })
