96ef78e12c2aec4a4d4c253e54da1dbe7bca3d5e
evolify <wolf@evolify.cn>
2025-07-11 11:32:37 +0800

    chore: ignore .next

--- .next/static/chunks/pages/_app-36fbc5cda7d48efb.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{73216:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(16332)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[593,792],()=>(n(73216),n(91050))),_N_E=_.O()}]);
